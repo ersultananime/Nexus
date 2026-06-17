90b6aa9880e92545c454ff06acce0ae88d582211
ersultananime <68365448+ersultananime@users.noreply.github.com>
2026-06-17 08:31:35 +0500

    remove backend-express

--- backend-express/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-const { validateBody, userCreateSchema } = require("../middleware/validate");
-
-router.post("/register", validateBody(userCreateSchema), authController.register);
-router.post("/login", authController.login);
-
-module.exports = router;
